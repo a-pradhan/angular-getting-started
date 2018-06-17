a67f11b6e7a69b7c84811a25c8663b5b1e382c43
Aditya Pradhan <apradhan@live.co.uk>
2018-06-17 04:02:39 +0100

    Implemented basic star rating feature

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit{
     imageMargin: number = 2
     showImage: boolean = false;
     _listFilter: string;
+    
     get listFilter(): string {
         return this._listFilter;
     }
--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'pm-star',
-    templateUrl: './star.componenent.html',
+    templateUrl: './star.component.html',
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    rating: number = 4;
+    @Input() rating: number;
     starWidth: number;
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 86/5;
+    }
+
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+    onClick(): void {
+        console.log(`The rating ${this.rating} was clicked!`)
     }
 
 }

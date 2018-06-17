5c6d622e78027a3f2f7204d6c8146109ce5c47a1
Aditya Pradhan <apradhan@live.co.uk>
2018-06-17 23:08:51 +0100

    implemented product service

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -13,6 +14,20 @@ export class ProductListComponent implements OnInit{
     showImage: boolean = false;
     _listFilter: string;
     
+    // long syntax
+
+    //private _productService;
+    // constructor(productService: ProductService) {
+    //     this.filteredProducts = this.products;
+    //     this.listFilter = 'cart';
+    //     this._productService = productService
+    // }
+
+    //shorthand syntax
+    constructor(private _productService: ProductService) {
+
+    }
+    
     get listFilter(): string {
         return this._listFilter;
     }
@@ -22,33 +37,7 @@ export class ProductListComponent implements OnInit{
         this.filteredProducts = this.listFilter? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[]
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ]
-
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+    products: IProduct[] = []
 
     onRatingClicked(message: string): void {
         this.pageTitle='Product List: ' + message;
@@ -65,6 +54,7 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log('In OnInit')
+        this.products = this._productService.getProducts()
+        this.filteredProducts = this.products;
     }
 }

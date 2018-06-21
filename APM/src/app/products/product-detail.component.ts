ac89ed05f70be042b96d284596daea32f36166c8
Aditya Pradhan <apradhan@live.co.uk>
2018-06-22 00:33:05 +0100

    Passing route parameters to product details page

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduct } from './product'
 
 @Component({
@@ -7,10 +8,24 @@ import { IProduct } from './product'
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail'
+  product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute ) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id'); // converts param to numeric type
+    this.pageTitle += `: ${id}`;
+    this.product =     {
+      "productId": id,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    }
   }
 
 }

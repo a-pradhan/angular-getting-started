5b4ae1a3a6150376c632aee4111cd32d74e8ae92
Aditya Pradhan <apradhan@live.co.uk>
2018-06-25 00:54:44 +0100

    implemented route guard for product detail page

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product'
 
 @Component({
@@ -11,7 +11,7 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail'
   product: IProduct;
 
-  constructor(private _route: ActivatedRoute ) { }
+  constructor(private _route: ActivatedRoute, private _router: Router ) { }
 
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id'); // converts param to numeric type
@@ -26,6 +26,11 @@ export class ProductDetailComponent implements OnInit {
       "starRating": 4.8,
       "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
     }
+
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
